7440d49dc2aaa9183e200acf83a845d5fc1121b3
ifeelbored <ursula771023@gmail.com>
2021-06-01 16:16:28 +0200

    initial react

---
+++ chapitre_01/elsy/src/Box.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './styles/global.css'
+
+class Box extends React.Component {
+
+  render() {
+    return (
+      <div className="box col-sm-3 col-6">
+        <span className="material-icons" style={{color: this.props.color, fontSize: 100 }} >{this.props.icon}</span>
+        <p>{this.props.value}
+        {this.props.unit}</p>
+        
+      </div>
+    );
+  }
+}
+
+export default Box;
