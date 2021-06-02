31f382df29f05f6dbff93203288c51b521b815ee
Wen-tzu LO <wlo@e2r10p16.42.fr>
2021-06-02 16:32:18 +0200

    Elsy (part 2)

--- chapitre_01/elsy/src/App.jsx
+++ chapitre_01/elsy/src/App.jsx
@@ -1,47 +1,86 @@
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
 import React from "react";
 import Box from "./Box.jsx";
 import "./styles/global.css";
-  const tempMin = -20;
-  const tempMax = 40;
-  const heartMin = 80;
-  const heartMax = 180;
-  const stepsMin = 0;
-  const stepsMax = 50000;
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      water: 1.5,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    };
+  }
+  calculateWater = () => {
+    let wateramount = 1.5;
+    if (this.state.temperature > 20) {
+      wateramount = wateramount + (this.state.temperature - 20) * 0.02;
+    }
+    if (this.state.heart > 120) {
+      wateramount = wateramount + (this.state.heart - 120) * 0.0008;
+    }
+    if (this.state.steps > 120) {
+      wateramount = wateramount + (this.state.heart - 10000) * 0.00002;
+    }
+    //console.log(wateramount);
+    wateramount = wateramount.toFixed(2);
+    this.setState({ water: wateramount });
+  };
+
+  onStepsChange = (e) => {
+    this.setState({ steps: e.target.value }, this.calculateWater);
+  };
+  onHeartChange = (e) => {
+    this.setState({ heart: e.target.value }, this.calculateWater);
+  };
+  onTempChange = (e) => {
+    this.setState({ temperature: e.target.value }, this.calculateWater);
+  };
+
   render() {
     return (
-      <div className="container-fluid" >
-        <div className="row" >
-        <Box icon="directions_walk" color="#3A85FF" value="1.5" unit ="L" />
-        <Box icon="local_drink" color="black" value="3000" unit ="steps"/>
-        <Box icon="favorite" color="red" value="200" unit ="bpm" />
-        <Box icon="wb_sunny" color="yellow" value="-10" unit ="°C"/>
+      <div className="container-fluid">
+        <div className="row">
+          <Box
+            icon="local_drink"
+            color="#3A85FF"
+            value={this.state.water}
+            unit="L"
+          />
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={this.state.steps}
+            unit="steps"
+            min={stepsMin}
+            max={stepsMax}
+            onChange={this.onStepsChange}
+          />
+          <Box
+            icon="favorite"
+            color="red"
+            value={this.state.heart}
+            unit="bpm"
+            min={heartMin}
+            max={heartMax}
+            onChange={this.onHeartChange}
+          />
+          <Box
+            icon="wb_sunny"
+            color="yellow"
+            value={this.state.temperature}
+            unit="°C"
+            min={tempMin}
+            max={tempMax}
+            onChange={this.onTempChange}
+          />
         </div>
       </div>
     );
--- chapitre_01/elsy/src/Box.jsx
+++ chapitre_01/elsy/src/Box.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import './styles/global.css'
+import "./styles/global.css";
 
 class Box extends React.Component {
-
   render() {
+    let x;
+    if (this.props.unit !== "L") {
+      x = (
+        <input
+          type="range"
+          min={this.props.min}
+          max={this.props.max}
+          onChange={this.props.onChange}
+          value={this.props.value}
+          step={this.props.icon === "directions_walk" ? 1000: 1}
+        />
+      );
+    }
     return (
-      <div className="box col-sm-3 col-6">
-        <span className="material-icons" style={{color: this.props.color, fontSize: 100 }} >{this.props.icon}</span>
-        <p>{this.props.value}
-        {this.props.unit}</p>
-        
+      <div className="box col-sm-3 col-6">
+        <span
+          className="material-icons"
+          style={{ color: this.props.color, fontSize: 100 }}
+        >
+          {this.props.icon}
+        </span>
+        <p>
+          {this.props.value}
+          {this.props.unit}
+        </p>
+        {x}
       </div>
     );
   }
